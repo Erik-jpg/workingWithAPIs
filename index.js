06da5f1a463d66fad8d21486f367bd34fdce42d3
Erik Stone <spologas@gmail.com>
2021-07-07 22:40:56 -0700

    class activites from 07/07/21

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-//Need name, location, bio, LinkedIn url, and GitHub URL.
-fs = require('fs');
-fs.writeFile('users.json', inquirer, [callback]);
-const inquirer = require('inquirer');
+// //Need name, location, bio, LinkedIn url, and GitHub URL.
+// fs = require('fs');
+// fs.writeFile('users.json', inquirer, [callback]);
+// const inquirer = require('inquirer');
 
-inquirer
-.prompt([
-    {
-    type: `input`,
-    message: 'What is your name?',
-    name: `username`,
-},
-{
-    type: 'input',
-    message: `Where are you located?`,
-    location: `location`,
-},
-{
-    type: 'input',
-    message: `Would mind telling me about yourself?`,
-    bio: `bio`,
-},
-{
-    type: 'input',
-    message: `What is your LinkedIn URL?`,
-    LinkedIn: `linkedIn URL`,
-},
-{
-    type: 'input',
-    message: `What is your GitHub URL?`,
-    GitHub: `GitHub URL`,
-}
-])
+// inquirer
+// .prompt([
+//     {
+//     type: `input`,
+//     message: 'What is your name?',
+//     name: `username`,
+// },
+// {
+//     type: 'input',
+//     message: `Where are you located?`,
+//     location: `location`,
+// },
+// {
+//     type: 'input',
+//     message: `Would mind telling me about yourself?`,
+//     bio: `bio`,
+// },
+// {
+//     type: 'input',
+//     message: `What is your LinkedIn URL?`,
+//     LinkedIn: `linkedIn URL`,
+// },
+// {
+//     type: 'input',
+//     message: `What is your GitHub URL?`,
+//     GitHub: `GitHub URL`,
+// }
+// ])
 
-.then((response) => {
-    console.log(response);
-    fs.appendFile('./html.html', JSON.stringify(response), (err) => console.log(err))
-});
+// .then((response) => {
+//     console.log(response);
+//     fs.appendFile('./html.html', JSON.stringify(response), (err) => console.log(err))
+// });
 
-const h1 = document.querySelector('#h1');
-const h2 = document.querySelector('#h2');
-const h3 = document.querySelector('#h3');
-const h4 = document.querySelector('#h4');
-const h5 = document.querySelector('#h5');
+// const h1 = document.querySelector('#h1');
+// const h2 = document.querySelector('#h2');
+// const h3 = document.querySelector('#h3');
+// const h4 = document.querySelector('#h4');
+// const h5 = document.querySelector('#h5');
 
-const makeHtml = (response) => {
+// const makeHtml = (response) => {
 
-}
+// }
 
-h1.value=`${username}`;
-h2.value=`${location}`;
-h3.value=`${bio}`;
-h4.value=`${LinkedIn}`;
-h5.value=`${GitHub}`;
+// h1.value=`${username}`;
+// h2.value=`${location}`;
+// h3.value=`${bio}`;
+// h4.value=`${LinkedIn}`;
+// h5.value=`${GitHub}`;
 
-console.log(fs);
+// console.log(fs);
 
 // const inquirer = require('inquirer');
 // inquirer
@@ -117,3 +117,65 @@ console.log(fs);
 // <script src="./script.js"></script>
 // </body>
 // </html>`
+
+// class Student {
+//     constructor(name){
+//         this.name = name
+//     }
+//     printName(){
+//         console.log(this.name)
+//     }
+// }
+// const alice = new Student('alice');
+// const brian = new Student('brian');
+
+// console.log(alice);
+// console.log(brian);
+
+// alice.printName();
+// brian.printName();
+
+// class Recipes {
+//     constructor(name, ingredients, cookingInstructions, temperatures) {
+//         this.name = name;
+//         this.ingredients = ingredients;
+//         this.cookingInstructions = cookingInstructions;
+//         this.temperatures = temperatures;
+//     }
+//     printInfo() {
+//         console.log(`name: ${this.name} \ningredients: ${this.ingredients} \ncookingInstructions: ${this.cookingInstructions} \n temperatures: ${this.temperatures}`)
+//     }
+// }
+// const RoastBeef = new Recipes({name: 'RoastBeef', ingredients: ['2lbs Chuck Roast', '4 large Carrots', '2 Yellow Onions', '2 large Russet potatoes'], cookingInstructions: ['Sear off roast', 'peel and chop carrots, potatoes, and onions', 'after roast is seared, toss all into a crock pot and set to cook for 4 hours'], temperatures:'set the crock pot temp to low'}); 
+
+
+// class Developer {
+//     constructor(name, tech) {
+//         this.name = name;
+//         this.tech = tech;
+//     }
+//     introduction() {
+//         console.log(`name: ${this.name} \n tech: ${this.tech}`)
+//     }
+// }
+
+class Pizza {
+    constructor({name, cheese, sauce, rating}){
+        this.name = name;
+        this.cheese = cheese;
+        this.sauce = sauce;
+        this.rating = rating;
+    }
+    printInfo(){
+        console.log(`name: ${this.name} \ncheese: ${this.cheese} \nsauce: ${this.sauce} \nrating: ${this.rating}`)
+    }
+    addIngredient(ingredients){
+        if (!ingredients.includes('')) {
+            this.ingredients.push(ingredients);    
+        }
+    }
+}
+
+const margarita = new Pizza({name: 'Margarita', ingredients: ['bread', 'cheese'], rating: '4/6'})
+margarita.printInfo()
+
