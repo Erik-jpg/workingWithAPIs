6a5a35d840bf87682da78ebadb2d5ff1eff3e470
Erik Stone <spologas@gmail.com>
2021-07-24 12:36:16 -0700

    LF will be replaced by CRLF in package-lock.JSON

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
-const http = require('http');
-const PORT = 8080;
+// const http = require('http');
+// const fs = require('fs');
+
+// const PORT = 8080;
+
+
+
+// const handleRequest = (req, res) => {
+//     fs.readFile(`${__dirname}/html.html`, (err, data) => {
+//         if (err) throw err;
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end(data);
+    
+//   });
+// };
+// //this is for the Favorite Foods Page
+// const displayFavoriteFood = (req, res) => {
+//     fs.readFile(`${__dirname}/html.html`, (err, data) => {
+//         res.writeHead(200, { 'Content-Type': 'text/html'});
+//         res.end(data);
+//     });
+// };
+// //this is for the Favorite Movies page
+// const displayFavoriteMovie = (req, res) => {
+//     fs.readFile(`${__dirname}${req.url}.html`, (err, data) => {
+//         res.writeHead(200, { 'Content-Type': 'text/html'});
+//         res.end(data);
+//     });
+// };
+// //this is for the Favorite CSS Framework Page
+// const displayFavoriteCSSFramework = (req, res) => {
+//     fs.readFile(`${__dirname}/html.html`, (err, data) => {
+//         res.writeHead(200, { 'Content-Type': 'text/html'});
+//         res.end(data);
+//     });
+// };
+
+// switch (path) {
+//     case '/displayFavoriteFood':
+//         return displayFavoriteFood(res);
+
+//     case '/displayFavoriteMovie':
+//         return displayFavoriteMovieMovie(res);
+
+//     case '/displayFavoriteCSSFramework':
+//         return displayFavoriteCSSFramework(res);
+// }
+
+// const server = http.createServer(handleRequest);
+
+// const path = request.url;
+
+// server.listen(PORT, () => {
+//     console.log(`Server is listening on PORT: ${PORT}`);
+//   });
+
+//First Express server!
+
---
+++ server.js
@@ -0,0 +1,40 @@
+// const express = require('express')
+// const app = express()
+
+
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World!')
+// })
+
+
+
+// app.get('/', (req, res)=> res.send('This is a catch all'));
+
+// app.listen(3000)
+
+// app.get('/api/terms/:term/:secondTerm/:weather', (req, res)=> {
+//     res.send(`the param you searched for is: ` ,  ${req.params.weather})
+// });
+
+// const { response } = require('express')
+// const express = require('express')
+// const app = express()
+// const PORT = 87650
+// fs = require('fs')
+// const pets = [];
+
+// app.get('/', (reqest, response) => {
+//     response.send()
+// })
+
+// app.post('/', (request, response) => {
+// response.send()
+// }) 
+
+// app.listen(PORT, () => {
+//     console.log(`Server is listening for pets at ${PORT}`)
+// });
+
+
+
